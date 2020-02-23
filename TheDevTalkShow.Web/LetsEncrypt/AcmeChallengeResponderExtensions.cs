31b0594ec592d22d6c3ffdb883bac9abdbb21ef0
Chris Gomez <cagomez@cellbycell.com>
2020-02-23 11:38:04 -0500

    Added AcmeChallengeResponder for LetsEncrypt
    ASP.NET Core on Linux can't use a web.config / IIS handler to take care of the response that proves you control the website.

---
+++ TheDevTalkShow.Web/LetsEncrypt/AcmeChallengeResponderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+
+namespace Innofactor.Acme
+{
+    public static class AcmeChallengeResponderExtensions
+    {
+        private const string WellKnownFolder = ".well-known";
+        private const string WellKnownRequestPath = "/.well-known";
+        private const string WellKnownContentType = "text/plain";
+
+        public static IApplicationBuilder UseAcmeChallengeResponder(this IApplicationBuilder app, string rootFolderPath)
+        {
+            if (rootFolderPath == null) throw new ArgumentNullException(nameof(rootFolderPath));
+
+            var root = new DirectoryInfo(rootFolderPath);
+            if (!root.Exists)
+            {
+                throw new ArgumentException("The provided folder does not exist");
+            }
+
+            var wellKnownFolder = root.CreateSubdirectory(WellKnownFolder);
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                RequestPath = WellKnownRequestPath,
+                FileProvider = new PhysicalFileProvider(wellKnownFolder.FullName),
+                ServeUnknownFileTypes = true,
+                DefaultContentType = WellKnownContentType
+            });
+
+            return app;
+        }
+    }
+}
--- TheDevTalkShow.Web/Startup.cs
+++ TheDevTalkShow.Web/Startup.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Innofactor.Acme;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace TheDevTalkShow.Web
 {
@@ -26,13 +32,15 @@ namespace TheDevTalkShow.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddMvc()
-                .AddNewtonsoftJson();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseAcmeChallengeResponder(env.ContentRootPath);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -40,23 +48,19 @@ namespace TheDevTalkShow.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(routes =>
-            {
-                routes.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                routes.MapRazorPages();
-            });
-
             app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
