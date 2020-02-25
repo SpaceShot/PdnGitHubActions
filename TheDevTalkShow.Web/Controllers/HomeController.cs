375992d0771ab1cd1cf85c112458295720875873
Chris Gomez <cagomez@cellbycell.com>
2020-02-25 08:49:47 -0500

    "Reboot" project for .NET Core SDK 3.1.101
    Previous commits did allow the project to compile and run under 3.1.1, it was becoming clear that the compatibility shims were going to lock us into "old" ways of doing things.  I did a File > New Project to see how the templates configure .NET Core now and transferred the views over.  Also implemented the Let's Encrypt challenge response.

--- TheDevTalkShow.Web/Controllers/HomeController.cs
+++ TheDevTalkShow.Web/Controllers/HomeController.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TheDevTalkShow.Web.Models;
 
 namespace TheDevTalkShow.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
         {
-            return View();
+            _logger = logger;
         }
 
-        public IActionResult About()
+        public IActionResult Index()
         {
             return View();
         }
@@ -25,10 +24,15 @@ namespace TheDevTalkShow.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
